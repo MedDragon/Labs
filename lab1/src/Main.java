8bcfbefe4c694d62ae1af08b212b21eac58faa5d
user <artem.karpenko@student.sumdu.edu.ua>
2023-09-28 14:15:36 +0300

    lab1 start 1

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Employee employee1 = new Employee("John", "Doe", 5000.0);
+            Employee employee1 = new Employee("Johnw", "Doe", 5000.0);
             employee1.getEmployeeInfo();
         } catch (FieldLengthLimitException | IncorrectSalaryException e) {
             System.out.println("Error: " + e.getMessage());
